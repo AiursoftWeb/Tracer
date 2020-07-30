83140f045bc7164a4fa3680a75d16ea1ea392714
Anduin Xue <anduin@aiursoft.com>
2020-07-30 21:30:05 +0800

    Better way to add caddy proxy.

--- install.sh
+++ install.sh
@@ -27,6 +27,18 @@ open_port()
     ufw reload
 }
 
+add_caddy_proxy()
+{
+    $domain_name="$1"
+    $local_port="$2"
+    cat /etc/caddy/Caddyfile | grep -q "an easy way" && echo "" > /etc/caddy/Caddyfile
+    echo "
+$domain_name {
+    reverse_proxy /* 127.0.0.1:$local_port
+}" >> /etc/caddy/Caddyfile
+    systemctl restart caddy.service
+}
+
 install_tracer()
 {
     server="$1"
@@ -83,12 +95,7 @@ install_tracer()
 
     # Config caddy
     echo 'Configuring the web proxy...'
-    echo "" > /etc/caddy/Caddyfile
-    echo "
-$server {
-    reverse_proxy /* 127.0.0.1:$port
-}" >> /etc/caddy/Caddyfile
-    systemctl restart caddy.service
+    add_caddy_proxy $server $port
 
     # Config firewall
     open_port 443
