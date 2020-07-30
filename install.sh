8045b1d05f8cbd5a2cf7505fcb3d59afaecdd021
Anduin Xue <anduin@aiursoft.com>
2020-07-30 22:04:41 +0800

    Fix a wrong caddy proxy add way.

--- install.sh
+++ install.sh
@@ -34,8 +34,8 @@ open_port()
 
 add_caddy_proxy()
 {
-    $domain_name="$1"
-    $local_port="$2"
+    domain_name="$1"
+    local_port="$2"
     cat /etc/caddy/Caddyfile | grep -q "an easy way" && echo "" > /etc/caddy/Caddyfile
     echo "
 $domain_name {
