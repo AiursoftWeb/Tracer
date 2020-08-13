d477ab1770472979771c058eda3df84ff5ec339f
Anduin Xue <anduin@aiursoft.com>
2020-08-13 14:09:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,12 @@ enable_bbr()
 
 set_production()
 {
-    cat /etc/environment | grep -q "Production" || echo 'ASPNETCORE_ENVIRONMENT="Production"' | tee -a /etc/environment
+    cat /etc/environment | grep -q "ASPNETCORE_ENVIRONMENT" || echo 'ASPNETCORE_ENVIRONMENT="Production"' | tee -a /etc/environment
+    cat /etc/environment | grep -q "DOTNET_CLI_TELEMETRY_OPTOUT" || echo 'DOTNET_CLI_TELEMETRY_OPTOUT="1"' | tee -a /etc/environment
+    cat /etc/environment | grep -q "DOTNET_PRINT_TELEMETRY_MESSAGE" || echo 'DOTNET_PRINT_TELEMETRY_MESSAGE="false"' | tee -a /etc/environment
+    export DOTNET_PRINT_TELEMETRY_MESSAGE="false"
     export ASPNETCORE_ENVIRONMENT="Production"
+    export DOTNET_CLI_TELEMETRY_OPTOUT=1
 }
 
 get_port()
