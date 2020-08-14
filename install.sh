6d9448427cd2919863ab75740bdb209982d81e4f
Anduin Xue <xuef@microsoft.com>
2020-08-14 15:54:18 +0800

    Remove get port.

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://github.com/AiursoftWeb/AiurScript/raw/master/$1.sh | sudo bash -s $arg; }
 
-get_port()
-{
-    while true; 
-    do
-        local PORT=$(shuf -i 40000-65000 -n 1)
-        ss -lpn | grep -q ":$PORT " || echo $PORT && break
-    done
-}
-
 install_tracer()
 {
     cd ~
