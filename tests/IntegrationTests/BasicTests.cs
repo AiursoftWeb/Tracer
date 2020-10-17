ef9e7fcc5c315ac0039e0ba6ace072faa5951bad
Anduin Xue <anduin.xue@microsoft.com>
2020-10-17 20:29:39 +0800

    Add test for tracer.

---
+++ tests/IntegrationTests/BasicTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Tracer.Tests.IntegrationTests
+{
+    public class BasicTests : IClassFixture<TracerFactory<Startup>>
+    {
+        private readonly TracerFactory<Startup> _factory;
+
+        public BasicTests(TracerFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task GetHome(string url)
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+            Assert.Contains("simple network quality testing app", content);
+        }
+    }
+}
---
+++ tests/TracerFactory.cs
@@ -0,0 +1,22 @@
+﻿using Aiursoft.Archon.SDK;
+using Aiursoft.Observer.SDK;
+using Aiursoft.Scanner;
+using Aiursoft.SDK;
+using Aiursoft.SDK.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tracer.Tests
+{
+    public class TracerFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => 
+            {
+                services.AddLibraryDependencies();
+            });
+        }
+    }
+}
