ff63ab35cee947dae2e5c18235a36c1dcab6d21d
Anduin Xue <anduin.xue@microsoft.com>
2020-10-17 20:34:07 +0800

    Remover and sort usings.

--- tests/IntegrationTests/BasicTests.cs
+++ tests/IntegrationTests/BasicTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace Tracer.Tests.IntegrationTests
--- tests/TracerFactory.cs
+++ tests/TracerFactory.cs
@@ -1,11 +1,6 @@
-﻿using Aiursoft.Archon.SDK;
-using Aiursoft.Observer.SDK;
-using Aiursoft.Scanner;
-using Aiursoft.SDK;
-using Aiursoft.SDK.Services;
+﻿using Aiursoft.Scanner;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Tracer.Tests
 {
