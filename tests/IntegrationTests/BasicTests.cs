0b899cd3dbcc7c5eb1da5e5bdbdc1b53edd0db49
Anduin2017 <anduin.xue@microsoft.com>
2020-10-18 13:34:47 +0800

    Fill more tests.

--- tests/IntegrationTests/BasicTests.cs
+++ tests/IntegrationTests/BasicTests.cs
@@ -14,6 +14,8 @@ namespace Tracer.Tests.IntegrationTests
 
         [Theory]
         [InlineData("/")]
+        [InlineData("/hOmE?aaaaaa=bbbbbb")]
+        [InlineData("/hOmE/InDeX")]
         public async Task GetHome(string url)
         {
             var client = _factory.CreateClient();
@@ -23,6 +25,19 @@ namespace Tracer.Tests.IntegrationTests
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
             Assert.Contains("simple network quality testing app", content);
+        }
+
+        [Theory]
+        [InlineData("/home/download")]
+        public async Task GetDownload(string url)
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/octet-stream", response.Content.Headers.ContentType.ToString());
+            Assert.Equal(1024 * 1024, content.Length);
         }
     }
 }
