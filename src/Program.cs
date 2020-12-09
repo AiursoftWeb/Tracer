a459ecf56bec2bb7d5c391f0eb2e97c8b7cdbe9d
Anduin Xue <anduin.xue@microsoft.com>
2020-12-09 14:42:14 +0800

    Higher code coverage.

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics.CodeAnalysis;
 using static Aiursoft.WebTools.Extends;
 
 namespace Tracer
 {
+    [ExcludeFromCodeCoverage]
     public class Program
     {
         public static void Main(string[] args)
