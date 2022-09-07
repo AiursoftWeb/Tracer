f078245f2146d82604be330871133b6b4a796fa9
Anduin Xue <xuef@microsoft.com>
2022-09-07 17:22:23 +0800

    Show more detailed information.

---
+++ src/Services/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System;
+
+namespace Tracer.Services
+{
+    public static class Extensions
+    {
+        public static string TryGetFullOSVersion()
+        {
+            var osVer = Environment.OSVersion;
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return osVer.VersionString;
+
+            try
+            {
+                var currentVersion = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+                if (currentVersion != null)
+                {
+                    var name = currentVersion.GetValue("ProductName", "Microsoft Windows NT");
+                    var ubr = currentVersion.GetValue("UBR", string.Empty)?.ToString();
+                    if (!string.IsNullOrWhiteSpace(ubr))
+                    {
+                        return $"{name} {osVer.Version.Major}.{osVer.Version.Minor}.{osVer.Version.Build}.{ubr}";
+                    }
+                }
+            }
+            catch
+            {
+                return osVer.VersionString;
+            }
+
+            return osVer.VersionString;
+        }
+    }
+}
