b70897465883c26472fa3cbac32ae9675cdfa5d0
Anduin Xue <anduin@aiursoft.com>
2023-12-30 14:34:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://gitlab.aiursoft.cn/aiursoft/aiurscript/-/raw/master/$1.sh | sudo bash -s $arg; }
 
-app_path="/opt/apps/Tracer"
+app_name="tracer"
+repo_path="https://gitlab.aiursoft.cn/aiursoft/tracer"
+proj_path="/tmp/repo/src/Aiursoft.Tracer.csproj"
+dll_name="Aiursoft.Tracer.dll"
 
 install()
 {
-    port=$(aiur network/get_port) && echo "Using internal port: $port"
+    echo "Installing $app_name..."
+    port=$(aiur network/get_port) && echo "Using port: $port"
+    
     aiur install/dotnet
-    aiur git/clone_to https://gitlab.aiursoft.cn/aiursoft/tracer /tmp/repo
-    aiur dotnet/publish $app_path /tmp/repo/src/Aiursoft.Tracer.csproj
-    aiur services/register_aspnet_service "tracer" $port $app_path "Aiursoft.Tracer.dll"
+    aiur git/clone_to $repo_path /tmp/repo
 
-    echo "Successfully installed Tracer as a service in your machine! Please open 0.0.0.0:$port to try it now!"
+    app_path="/opt/apps/$app_name"
+    aiur dotnet/publish $app_path $proj_path
+    aiur services/register_aspnet_service $app_name $port $app_path $dll_name
+
+    echo "Install $app_name finished! Please open $(hostname):$port to try!"
     rm ./tmp/repo -rf
 }
 
