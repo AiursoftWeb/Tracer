cbf95f428208e7ab218b76b56fe519700c6de557
Anduin Xue <anduin@aiursoft.com>
2023-12-30 14:27:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ install()
     port=$(aiur network/get_port) && echo "Using internal port: $port"
     aiur install/dotnet
     aiur git/clone_to https://gitlab.aiursoft.cn/aiursoft/tracer /tmp/repo
-    aiur dotnet/publish $tracer_path /tmp/repo/src/Aiursoft.Tracer.csproj
-    aiur services/register_aspnet_service "tracer" $port $tracer_path "Aiursoft.Tracer"
+    aiur dotnet/publish $app_path /tmp/repo/src/Aiursoft.Tracer.csproj
+    aiur services/register_aspnet_service "tracer" $port $app_path "Aiursoft.Tracer.dll"
 
     echo "Successfully installed Tracer as a service in your machine! Please open 0.0.0.0:$port to try it now!"
     rm ./tmp/repo -rf
