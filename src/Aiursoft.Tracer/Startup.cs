1962c5835b9a2e018806788e891f105ef10e4927
Anduin Xue <anduin@aiursoft.com>
2026-07-26 17:57:13 +0000

    Migrate to Aiursoft.UiStack 10.0.22 - embedded static assets

--- src/Aiursoft.Tracer/Startup.cs
+++ src/Aiursoft.Tracer/Startup.cs
@@ -8,6 +8,7 @@ using Aiursoft.Tracer.InMemory;
 using Aiursoft.Tracer.MySql;
 using Aiursoft.Tracer.Services.Authentication;
 using Aiursoft.Tracer.Sqlite;
+using Aiursoft.UiStack;
 using Aiursoft.UiStack.Layout;
 using Aiursoft.UiStack.Navigation;
 using Microsoft.AspNetCore.Mvc.Razor;
@@ -76,6 +77,7 @@ public class Startup : IWebStartup
     {
         app.UseExceptionHandler("/Error/Code500");
         app.UseStatusCodePagesWithReExecute("/Error/Code{0}");
+        app.UseUIStack();
         app.UseStaticFiles();
         app.UseRouting();
         app.UseAuthentication();
